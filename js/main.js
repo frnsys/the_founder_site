2aafe8e4c401631dd457a1605a11528d73402cc8
Francis Tseng <f+accounts@frnsys.com>
2017-01-25 19:13:34 -0500

    updates

--- js/main.js
+++ js/main.js
@@ -1,42 +1,31 @@
-require(['config'], function() {
-    'use strict';
+function setup_colors() {
+    var raw_colors = [
+        '#e4e4e4',
+        '#c5fafe',
+        '#fff8ac',
+        '#ffb9ad',
+        '#A77BFA',
+        '#7DC7F6',
+        '#B7CEF8',
+        '#c7ffe7',
+        '#e4e4e4',
+    ];
+    if ($(window).width() <= 480) {
+        raw_colors = ['#C7F8FE'];
+    }
 
-    require([
-            'jquery',
-            'modernizr',
-            'colorscroll'
-    ], function($, ø) {
-        function setup_colors() {
-            var raw_colors = [
-                '#e4e4e4',
-                '#c5fafe',
-                '#fff8ac',
-                '#ffb9ad',
-                '#A77BFA',
-                '#7DC7F6',
-                '#B7CEF8',
-                '#c7ffe7',
-                '#e4e4e4',
-            ];
-            if ($(window).width() <= 480) {
-                raw_colors = ['#C7F8FE'];
-            }
+    var colors = [];
+    for (var i=0; i<raw_colors.length; i++) {
+        colors.push({
+            color: raw_colors[i],
+            position: $(window).height() * i
+        });
+    };
 
-            var colors = [];
-            for (var i=0; i<raw_colors.length; i++) {
-                colors.push({
-                    color: raw_colors[i],
-                    position: $(window).height() * i
-                });
-            };
-
-            $('body').colorScroll({
-                colors: colors
-            });
-        }
-
-        setup_colors();
-        $(window).resize(setup_colors);
+    $('body').colorScroll({
+        colors: colors
     });
+}
 
-});
+setup_colors();
+$(window).resize(setup_colors);
